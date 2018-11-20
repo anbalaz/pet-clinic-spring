43b7a09cf1da013ab56351823a660750ae226e13
anbalaz <anbalaz@gmail.com>
2018-11-20 16:49:50 +0100

    adding POJOs

---
+++ src/main/java/com/balaz/petclinicspring/model/Owner.java
@@ -0,0 +1,5 @@
+package com.balaz.petclinicspring.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/balaz/petclinicspring/model/Vet.java
@@ -0,0 +1,5 @@
+package com.balaz.petclinicspring.model;
+
+public class Vet extends Person {
+
+}
