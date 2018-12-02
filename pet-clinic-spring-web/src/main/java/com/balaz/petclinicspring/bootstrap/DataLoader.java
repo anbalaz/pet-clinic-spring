d72fdef56dffecda5ad9e337b37e6dc71c20c10a
anbalaz <anbalaz@gmail.com>
2018-12-02 16:23:22 +0100

    Repaired pom in pet-clinic-spring-data repackage-true.

--- pet-clinic-spring-web/src/main/java/com/balaz/petclinicspring/bootstrap/DataLoader.java
+++ pet-clinic-spring-web/src/main/java/com/balaz/petclinicspring/bootstrap/DataLoader.java
@@ -24,13 +24,13 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Owner owner1= new Owner();
+        Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
-        Owner owner2= new Owner();
+        Owner owner2 = new Owner();
         owner2.setFirstName("Samuel");
         owner2.setLastName("Lobotka");
 
@@ -39,20 +39,19 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
 
-        Vet vet1= new Vet();
+        Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Romualdo");
 
         vetService.save(vet1);
 
-        Vet vet2= new Vet();
+        Vet vet2 = new Vet();
         vet2.setFirstName("John");
         vet2.setLastName("Breed");
 
         vetService.save(vet2);
 
         System.out.println("Loaded Vets...");
-
 
 
     }
