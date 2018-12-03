9f49efcf88534629bcbacfe806b1e8e4e4bbf721
anbalaz <anbalaz@gmail.com>
2018-12-03 07:24:42 +0100

    Added Pets and Contact info to owners on startup, closes #33

--- pet-clinic-spring-web/src/main/java/com/balaz/petclinicspring/bootstrap/DataLoader.java
+++ pet-clinic-spring-web/src/main/java/com/balaz/petclinicspring/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.balaz.petclinicspring.bootstrap;
 
 import com.balaz.petclinicspring.model.Owner;
+import com.balaz.petclinicspring.model.Pet;
 import com.balaz.petclinicspring.model.PetType;
 import com.balaz.petclinicspring.model.Vet;
 import com.balaz.petclinicspring.services.OwnerService;
@@ -9,6 +10,8 @@ import com.balaz.petclinicspring.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -31,22 +34,42 @@ public class DataLoader implements CommandLineRunner {
 
         PetType dog = new PetType();
         dog.setName("Dog");
-        PetType saveDogPetType= petTypeService.save(dog);
+        PetType saveDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
         dog.setName("Cat");
-        PetType saveCatPetType= petTypeService.save(cat);
+        PetType saveCatPetType = petTypeService.save(cat);
 
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("Second Street 15");
+        owner1.setCity("Oklahoma");
+        owner1.setTelephone("09453123123");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(saveDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthday(LocalDate.now());
+        mikesPet.setName("Rex");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Samuel");
         owner2.setLastName("Lobotka");
+        owner2.setAddress("Beach Street 1");
+        owner2.setCity("New York");
+        owner2.setTelephone("0922321123");
+
+        Pet samuelsPet = new Pet();
+        samuelsPet.setPetType(saveCatPetType);
+        samuelsPet.setOwner(owner2);
+        samuelsPet.setBirthday(LocalDate.now());
+        samuelsPet.setName("Fiona");
+        owner2.getPets().add(samuelsPet);
 
         ownerService.save(owner2);
 
