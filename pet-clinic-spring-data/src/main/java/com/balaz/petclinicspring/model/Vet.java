871d02a834ccb5ccd57727fd50320cbed783623a
anbalaz <anbalaz@gmail.com>
2019-01-01 18:27:11 +0100

    Updated vet with JPA mappings, closes #37

--- pet-clinic-spring-data/src/main/java/com/balaz/petclinicspring/model/Vet.java
+++ pet-clinic-spring-data/src/main/java/com/balaz/petclinicspring/model/Vet.java
@@ -1,10 +1,16 @@
 package com.balaz.petclinicspring.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "person")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
