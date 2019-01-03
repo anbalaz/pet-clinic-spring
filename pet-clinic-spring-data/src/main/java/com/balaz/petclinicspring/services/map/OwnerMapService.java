4cbf8a7ba962f4d763cf33e7ffc2686bf64565ca
anbalaz <anbalaz@gmail.com>
2019-01-03 21:57:46 +0100

    Adding CRUD tests for OwnerMapService, cleaning poms(deleting surefire plugin, which is already build) closes #49

--- pet-clinic-spring-data/src/main/java/com/balaz/petclinicspring/services/map/OwnerMapService.java
+++ pet-clinic-spring-data/src/main/java/com/balaz/petclinicspring/services/map/OwnerMapService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-@Profile({"default","map"})
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
@@ -34,24 +34,23 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        if (object.getPets() != null) {
-            object.getPets().forEach(pet -> {
-                if (pet.getPetType() != null) {
-                    if (pet.getPetType().getId() == null) {
-                        pet.setPetType(petTypeService.save(pet.getPetType()));
-                    }
-                } else {
-                    throw new RuntimeException("Pet type is required!");
-                }
-
-                if (pet.getId() == null) {
-                    Pet savedPet = petService.save(pet);
-                    pet.setId(savedPet.getId());
-                }
-            });
-        }
-
         if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet type is required!");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
 
             return super.save(object);
         } else {
@@ -72,7 +71,18 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
     }
 
     @Override
-    public Owner findByLastName(String LastName) {
-        return null;
+    public Owner findByLastName(String lastName) {
+//        for (Owner owner : this.findAll()) {
+//            if (owner.getLastName().equalsIgnoreCase(lastName)) {
+//                return owner;
+//            }
+//        }
+//        return null;
+//    }
+        return this.findAll()
+                .stream()
+                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+                .findFirst()
+                .orElse(null);
     }
 }
